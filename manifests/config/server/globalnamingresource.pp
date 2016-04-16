3a2996942582a99e0db46f67eb3d5abe29c45a7f
Neil Prockter <prockter@lse.ac.uk>
2016-04-16 13:42:36 +0100

    debug

--- manifests/config/server/globalnamingresource.pp
+++ manifests/config/server/globalnamingresource.pp
@@ -49,7 +49,7 @@ define tomcat::config::server::globalnamingresource (
     $changes = delete_undef_values(flatten([ $_additional_attributes, $_attributes_to_remove ]))
   }
 
-  notify{"globalnamingreource changes ${changes}": }
+  file {"/tmp/globalnamingreource.aug': content=>$changes }
 
   augeas { "server-${catalina_base}-globalresource-${name}":
     lens    => 'Xml.lns',
